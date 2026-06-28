556e716108904e9b6cc0e26cc51b816f8cefe5e7
ekappzone <ekappzone@gmail.com>
2026-06-28 12:33:34 +0000

    Router imported in config/urls.py1

--- dashboard/config/urls.py
+++ dashboard/config/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
+from api.router import router  
 
 urlpatterns = [
     # Admin
